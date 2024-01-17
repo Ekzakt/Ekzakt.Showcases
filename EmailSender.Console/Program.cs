6d168791995ed95c1fdff137c3bb82c0cc5ff68d
EricJansen <EricJansen@LAPTOP-EJ>
2024-01-17 19:27:53 +0100

    Refactored.

--- EmailSender.Console/Program.cs
+++ EmailSender.Console/Program.cs
@@ -2,7 +2,6 @@
 using Ekzakt.EmailSender.Smtp.Configuration;
 using Ekzakt.EmailSender.Core.Contracts;
 using Ekzakt.EmailSender.Core.Models;
-
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 
@@ -12,34 +11,52 @@ services.AddSmtpEmailSender();
 
 var host = Host
     .CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration(config => 
+    .ConfigureAppConfiguration(config =>
         config.AddJsonFile(
             path: "appsettings.Development.json",
-            optional: true, 
+            optional: false,
             reloadOnChange: true)
         )
     .ConfigureServices((context, services) =>
     {
-        services.AddSmtpEmailSender(options =>
-        {
-            options.FromDisplayName = "Console App";
-            options.FromAddress = "info@smoothsensation.net";
-            options.UserName = "info@smoothsensation.net";
-            options.Password = "hTioin_2nB";
-            options.Host = "smtp-auth.mailprotect.be";
-            options.Port = 587;
-        });         
+        services.AddSmtpEmailSender();
+        //services.AddSmtpEmailSender(options =>
+        //{
+        //    options.FromDisplayName = "Console App";
+        //    options.FromAddress = "info@smoothsensation.net";
+        //    options.UserName = "info@smoothsensation.net";
+        //    options.Password = "hTioin_2nB";
+        //    options.Host = "smtp-auth.mailprotect.be";
+        //    options.Port = 587;
+        //});
     })
     .Build();
 
 IEmailSenderService _emailService = host.Services.GetService<IEmailSenderService>();
 
-var request = new SendEmailRequest();
 
-request.Tos.Add(new EmailAddress("mail@ericjansen.com"));
-request.Subject = "Send from Console Application";
-request.HtmlBody = "<h1>Console Applications</h1>";
+while (true)
+{
+    Console.WriteLine("Send email? (Y)es (N)o");
+    ConsoleKeyInfo yesNo = Console.ReadKey(true);
 
-var result = await _emailService!.SendAsync(request);
+    if (yesNo.Key == ConsoleKey.N)
+    {
+        Console.WriteLine("Exiting...");
+        break;
+    }
 
-Console.WriteLine(result.ServerResponse);
+    var request = new SendEmailRequest();
+
+    request.Tos.Add(new EmailAddress("mail@ericjansen.com"));
+    request.Subject = "Send from Console Application";
+    request.HtmlBody = "<h1>Console Applications</h1>";
+
+    var result = await _emailService!.SendAsync(request);
+
+    Console.WriteLine(result.ServerResponse);
+}
+
+
+
+
